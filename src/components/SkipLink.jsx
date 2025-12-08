3f30f75fd11775a45ae2da3f245b17db4ca13b8c
einadid <emamulislamnadid_cse30d@portcity.edu.bd>
2025-12-08 17:04:33 +0600

    Remove all files

--- src/components/SkipLink.jsx
+++
@@ -1,8 +0,0 @@
-export default function SkipLink() {
-return (
-<a href="#main-content" className="sr-only focus:not-sr-only focus:fixed focus:top-2 focus:left-2 btn btn-sm">
-Skip to content
-</a>
-);
-}
-
